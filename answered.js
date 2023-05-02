94a15a729f8a5e67e04c98233f966200a9fe8b63
rahmeesha reynolds <crazym.weird@gmail.com>
2023-05-02 17:39:35 -0400

    "Fizz Buzz answered"

--- answered.js
+++ answered.js
@@ -12,13 +12,54 @@
 // }
 
 // Get Even //
-let grow = 0;
-while (grow <= 200) {
-    console.log(grow)
-    grow = grow + 2
+// let grow = 0;
+// while (grow <= 200) {
+//     console.log(grow)
+//     grow = grow + 2
+// }
+
+// Fizz Buzz //
+
+// Write a javascript application that logs all numbers from 1 - 100.
+// let mee = 0;
+// while (mee <= 100) {
+//     console.log(mee)
+//     mee = mee + 1
+// }
+
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// I'm not sure how to do this.
+let t = 3((90));
+if (t % 90) {
+    console.log("fizz");
 }
 
-////////////////////////// PART 1: HTML & CSS ////////////////////////////////////
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// I'm not sure how to do this.
+let 
+
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
 
 
-////////////////////////// PART 1: HTML & CSS ////////////////////////////////////
+
+// Wild Wild Life //
+
+
+
+// Yell at the Ninja Turtles //
+
+
+
+// Methods, Revised //
+
+
+
+// Where is Waldo //
+
+
+
+// Excited Kitten //
+
+
+
+// Find the Median //
